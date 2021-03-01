13b5753f76437e5f6e051115aba91ce909583321
notrexxx <notrexx15@gmail.com>
2021-03-01 19:15:03 -0400

    (bug) fix rspec test

--- spec/badgod_spec.rb
+++ spec/badgod_spec.rb
@@ -4,7 +4,7 @@ describe '.trailing_whitespace' do
   context 'When a trailing whitespace is detected' do
     it 'return trailing whitespace error' do
       var = "\e[0;31;49m./test/badtest.rb:1 : 1) Please remove trailing white space\n\e[0m"
-      dir = './test/badtest.rb '
+      dir = './test/badtest.rb'
       file = File.open(dir)
       file.each_with_index do |num, i|
         expect(Badgod.trailing_whitespace(dir, num, i)).to eql(var) if i == 0
@@ -17,7 +17,7 @@ describe '.nav' do
   context 'When spaces around a method call are detected' do
     it 'return spaces around a method call operator error' do
       var = "\e[0;31;49m./test/badtest.rb:3 : 2) Please  Avoid using spaces around a method call operator\n\e[0m"
-      dir = './test/badtest.rb '
+      dir = './test/badtest.rb'
       file = File.open(dir)
       file.each_with_index do |num, i|
         expect(Badgod.nav(dir, num, i)).to eql(var) if i == 2
@@ -30,7 +30,7 @@ describe '.colon' do
   context 'When unnecesary semicolons are detected' do
     it 'return linter for unnecesary semicolons error' do
       var = "\e[0;31;49m./test/badtest.rb:2 : 3) Please remove semicolon\n\e[0m"
-      dir = './test/badtest.rb '
+      dir = './test/badtest.rb'
       file = File.open(dir)
       file.each_with_index do |num, i|
         expect(Badgod.colon(dir, num, i)).to eql(var) if i == 1
@@ -43,7 +43,7 @@ describe '.comma' do
   context 'When a space after comma is detected' do
     it 'return linter for space after comma error' do
       var = "\e[0;31;49m./test/badtest.rb:5 : 4) Please add space after comma\n\e[0m"
-      dir = './test/badtest.rb '
+      dir = './test/badtest.rb'
       file = File.open(dir)
       file.each_with_index do |num, i|
         expect(Badgod.comma(dir, num, i)).to eql(var) if i == 4
@@ -56,7 +56,7 @@ describe '.indent_after' do
   context 'When a bad indentation is detected' do
     it 'return linter for 2 spaces only indentation error' do
       var = "\e[0;31;49m./test/badtest.rb:4 : 5) Please use 2 spaces for indentation.\n\e[0m"
-      dir = './test/badtest.rb '
+      dir = './test/badtest.rb'
       file = File.open(dir)
       file.each_with_index do |num, i|
         expect(Badgod.indent_after(dir, num, i)).to eql(var) if i == 3
@@ -69,7 +69,7 @@ describe '.empty' do
   context 'When a empty block is detected' do
     it 'return linter for empty block error' do
       var = "\e[0;31;49m./test/badtest.rb:5 : 6) Please add content to the block\n\e[0m"
-      dir = './test/badtest.rb '
+      dir = './test/badtest.rb'
       file = File.open(dir)
       file.each_with_index do |num, i|
         expect(Badgod.empty(dir, num, i)).to eql(var) if i == 4
@@ -82,7 +82,7 @@ describe '.colon_space' do
   context 'When a space after colon is detected' do
     it 'return linter for space after colon error' do
       var = "\e[0;31;49m./test/badtest.rb:4 : 7) Please add space after colon\n\e[0m"
-      dir = './test/badtest.rb '
+      dir = './test/badtest.rb'
       file = File.open(dir)
       file.each_with_index do |num, i|
         expect(Badgod.colon_space(dir, num, i)).to eql(var) if i == 3
--- spec/god_spec.rb
+++ spec/god_spec.rb
@@ -4,7 +4,7 @@ describe '.trailing_whitespace' do
   context 'When a trailing whitespace is detected' do
     it 'return trailing whitespace error' do
       var = "\e[0;31;49m./test/badtest.rb:1 : 1) Please remove trailing white space\n\e[0m"
-      dir = './test/badtest.rb '
+      dir = './test/badtest.rb'
       file = File.open(dir)
       file.each_with_index do |num, i|
         expect(God.trailing_whitespace(dir, num, i)).to eql(var) if i == 0
@@ -26,7 +26,7 @@ describe '.nav' do
   context 'When spaces around a method call are detected' do
     it 'return spaces around a method call operator error' do
       var = "\e[0;31;49m./test/badtest.rb:3 : 2) Please  Avoid using spaces around a method call operator\n\e[0m"
-      dir = './test/badtest.rb '
+      dir = './test/badtest.rb'
       file = File.open(dir)
       file.each_with_index do |num, i|
         expect(God.nav(dir, num, i)).to eql(var) if i == 2
@@ -48,7 +48,7 @@ describe '.colon' do
   context 'When unnecesary semicolons are detected' do
     it 'return linter for unnecesary semicolons error' do
       var = "\e[0;31;49m./test/badtest.rb:2 : 3) Please remove semicolon\n\e[0m"
-      dir = './test/badtest.rb '
+      dir = './test/badtest.rb'
       file = File.open(dir)
       file.each_with_index do |num, i|
         expect(God.colon(dir, num, i)).to eql(var) if i == 1
@@ -70,7 +70,7 @@ describe '.comma' do
   context 'When a space after comma is detected' do
     it 'return linter for space after comma error' do
       var = "\e[0;31;49m./test/badtest.rb:5 : 4) Please add space after comma\n\e[0m"
-      dir = './test/badtest.rb '
+      dir = './test/badtest.rb'
       file = File.open(dir)
       file.each_with_index do |num, i|
         expect(God.comma(dir, num, i)).to eql(var) if i == 4
@@ -92,7 +92,7 @@ describe '.indent_after' do
   context 'When a bad indentation is detected' do
     it 'return linter for 2 spaces only indentation error' do
       var = "\e[0;31;49m./test/badtest.rb:4 : 5) Please use 2 spaces for indentation.\n\e[0m"
-      dir = './test/badtest.rb '
+      dir = './test/badtest.rb'
       file = File.open(dir)
       file.each_with_index do |num, i|
         expect(God.indent_after(dir, num, i)).to eql(var) if i == 3
@@ -114,7 +114,7 @@ describe '.empty' do
   context 'When a empty block is detected' do
     it 'return linter for empty block error' do
       var = "\e[0;31;49m./test/badtest.rb:5 : 6) Please add content to the block\n\e[0m"
-      dir = './test/badtest.rb '
+      dir = './test/badtest.rb'
       file = File.open(dir)
       file.each_with_index do |num, i|
         expect(God.empty(dir, num, i)).to eql(var) if i == 4
@@ -136,7 +136,7 @@ describe '.colon_space' do
   context 'When a space after colon is detected' do
     it 'return linter for space after colon error' do
       var = "\e[0;31;49m./test/badtest.rb:4 : 7) Please add space after colon\n\e[0m"
-      dir = './test/badtest.rb '
+      dir = './test/badtest.rb'
       file = File.open(dir)
       file.each_with_index do |num, i|
         expect(God.colon_space(dir, num, i)).to eql(var) if i == 3
